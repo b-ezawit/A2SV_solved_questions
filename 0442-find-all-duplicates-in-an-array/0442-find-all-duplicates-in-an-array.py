86afa6ae2e0afc39567c5984d2dcd3331f523955
Bezawit Biazine <bezawitbiazine@gmail.com>
2026-02-06 10:22:16 +0300

    Time: 72 ms (19.05%), Space: 23.8 MB (46.03%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.py
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.py
@@ -7,4 +7,15 @@ class Solution(object):
         #brute force:
         numsCount = Counter(nums)
         return [k for k in numsCount if numsCount[k]==2]
-        
+
+        #elements in nums in range: [1,n]
+        #nums = [4,3,2,7,8,2,3,1]
+        #n=8, possible values=[1,2,3,4,5,6,7,8] 
+        #              index:  0 1 2 3 4 5 6 7
+        ans = []
+        for n in nums:
+            if nums[abs(n)-1] < 0:
+                ans.append(abs(n))
+            else:
+                nums[abs(n)-1] = -nums[abs(n)-1]
+        return ans
