923762fcaf861f7768c4f712cdd5ad1bb2398ec6
Bezawit Biazine <bezawitbiazine@gmail.com>
2026-02-21 09:46:55 -0500

    Time: 4 ms (14.72%), Space: 13.2 MB (7.95%) - LeetHub

---
+++ 0867-transpose-matrix/0867-transpose-matrix.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def transpose(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        rows = len(matrix)
+        cols = len(matrix[0])
+
+
+        res = [[0]*rows for _ in range(cols)]
+
+        for r in range(rows):
+            for c in range(cols):
+                res[c][r] = matrix[r][c]
+        return res
+        
+        
+        
