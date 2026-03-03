a0581ce853b2488de6e9273d611d850c8305d084
Bezawit Biazine <bezawitbiazine@gmail.com>
2026-03-03 04:07:45 -0500

    Time: 83 ms (63.87%), Space: 19.5 MB (91.49%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,28 +1,17 @@
-class Solution(object):
-    def characterReplacement(self, s, k):
-        """
-        :type s: str
-        :type k: int
-        :rtype: int
-        """
-        #k=3 s="ab"
-        left = right = maxlen = currlen = maj =0
-        hmap = defaultdict(int)
-        
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        # s = "AABABBA", k = 1
+        left = maj = maxlen = 0
+
+        hmap = defaultdict(int)#stores valid substring with the count of each char
+
         for right in range(len(s)):
-            hmap[s[right]] += 1
-            #if maj<hmap[c]:
-             #   maj = hmap[c]
-              #  majChar = c  
-            maj = max(maj, hmap[s[right]])          
-            while maj + k < right-left+1:
+            hmap[s[right]] += 1#hmap = {'A':1}
+            maj = max(maj,  hmap[s[right]])
+            while maj + k < right - left + 1:
                 hmap[s[left]] -= 1
-                #if majChar == s[left]:
-                #   maj -= 1
-                if hmap[s[left]] <= 0:
+                if hmap[s[left]]<=0:
                     del hmap[s[left]]
-                #maj = max(maj,hmap[s[left]])
                 left += 1
             maxlen = max(maxlen,right-left+1)
         return maxlen
-
