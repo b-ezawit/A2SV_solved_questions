e534413245b03a323107025574bf5648afd57962
Bezawit Biazine <bezawitbiazine@gmail.com>
2026-06-11 11:00:22 +0300

    Time: 4041 ms (56.76%), Space: 52.5 MB (52.25%) - LeetHub

--- 3691-maximum-total-subarray-value-ii/3691-maximum-total-subarray-value-ii.py
+++ 3691-maximum-total-subarray-value-ii/3691-maximum-total-subarray-value-ii.py
@@ -1,70 +1,50 @@
-from heapq import heappush, heappop
-
-
 class Solution:
-    def maxTotalValue(self, nums, k):
+    def maxTotalValue(self, nums: List[int], k: int) -> int:
         n = len(nums)
-        levels = n.bit_length()
-
-        maxs = [[0] * n for _ in range(levels)]
-
-        mins = [[0] * n for _ in range(levels)]
-
-        for i in range(n):
-            maxs[0][i] = nums[i]
-            mins[0][i] = nums[i]
-
-        level = 1
-        while (1 << level) <= n:
-            half = 1 << (level - 1)
-            for start in range(
-                n - (1 << level) + 1
-            ):
-                maxs[level][start] = max(
-                    maxs[level - 1][start],
-                    maxs[level - 1][start + half]
-                )
-                mins[level][start] = min(
-                    mins[level - 1][start],
-                    mins[level - 1][start + half]
-                )
-
-            level += 1
-        logs = [0] * (n + 1)
-
-        for length in range(2, n + 1):
-            logs[length] = logs[length // 2] + 1
-
-        def value(left, right):
-            length = right - left + 1
-            power = logs[length]
-            size = 1 << power
-            maximum = max(
-                maxs[power][left],
-                maxs[power][right - size + 1]
-            )
-
-            minimum = min(
-                mins[power][left],
-                mins[power][right - size + 1]
-            )
-
-            return maximum - minimum
-
         heap = []
-        for left in range(n):
-            right = n - 1
-            score = value(left, right)
-            heappush(heap,(-score, left, right))
-            
+        #spare tree
+        K = int(log2(n)) + 1
+        stMax  =  [[0] * K for _ in range(n)]     
+        stMin  =  [[0] * K for _ in range(n)]
 
-        total = 0
-        for _ in range(k):
-            score, left, right = heappop(heap)
-            total += -score
-            if right > left:
-                nxt = right - 1
-                score = value(left, nxt)
-                heappush( heap, (-score, left, nxt))
+        for i in range(n) :
+            stMax[i][0] = nums[i]
+            stMin[i][0] = nums[i]
 
-        return total
+        j = 1
+        while (1 << j) <= n :
+            i = 0
+            while i + (1<<j) <= n :
+                stMax[i][j] = max(stMax[i][j-1] , stMax[i + (1 << (j-1))][j-1])
+                stMin[i][j] = min(stMin[i][j-1] , stMin[i + (1 << (j-1))][j-1])
+
+                i += 1
+            j += 1
+        def queryMax(l,r) :
+            k = int(log2(r - l + 1))
+            return max(stMax[l][k] , stMax[r - (1 << k) + 1][k])
+
+        def queryMin(l,r) :
+            k = int(log2(r - l + 1))
+            return min(stMin[l][k] , stMin[r - (1 << k) + 1][k])
+
+        def getValue(l ,r) :
+            return queryMax(l,r) - queryMin(l,r)        
+
+        for l in range(n) :
+            value = getValue(l , n-1)
+            heapq.heappush(heap , (-value , l , n-1))
+
+        ans = 0
+        for _ in range(k) :
+            value , l , r = heapq.heappop(heap)
+            value = -value
+            ans += value
+
+            if r > l :
+                newVal = getValue(l , r-1)
+                heapq.heappush(heap , (-newVal , l , r-1))
+        return ans
+
+
+        
