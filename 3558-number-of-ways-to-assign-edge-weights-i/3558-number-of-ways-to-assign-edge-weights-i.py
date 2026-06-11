79cc62ac24d810409cbbdca61abfdb3fccf24270
Bezawit Biazine <bezawitbiazine@gmail.com>
2026-06-11 08:27:47 +0300

    Time: 377 ms (58.06%), Space: 131.8 MB (9.68%) - LeetHub

---
+++ 3558-number-of-ways-to-assign-edge-weights-i/3558-number-of-ways-to-assign-edge-weights-i.py
@@ -0,0 +1,21 @@
+class Solution:
+    def assignEdgeWeights(self, edges: List[List[int]]) -> int:
+        graph  = defaultdict(list)
+        for u,v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
+
+    
+        def dfs(node, parent, currDepth):
+            maxD = currDepth
+
+            for nei in graph[node]:
+                if nei != parent:
+                    maxD = max(maxD, dfs(nei,node,currDepth + 1))
+            return maxD
+        
+        maxDepth =  dfs(1,None,0)
+        MOD = (10**9)+7
+
+        return (2 ** (maxDepth - 1)) % MOD
+    
